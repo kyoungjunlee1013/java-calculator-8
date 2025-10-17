d203ffbb2150a1675f621d55adcb5cc61d2dd019
이경준 <kyoungjunlee@igyeongjun-ui-MacBookPro.local>
2025-10-17 14:52:13 +0900

    1. **문자열을 입력받고, 입력이 빈 문자열() 또는 null일 경우 0을 반환한다.**

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,5 +7,16 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String text = Console.readLine();
+
+        StringCalculator calculator = new StringCalculator();
+        try {
+            int result = calculator.add(text);
+
+            //계산된 값을 출력.
+            System.out.println("결과: " + result);
+        }catch (IllegalArgumentException e){
+            //StringCalculator에서 던지는 예외 처리를 하는 곳.
+            System.out.println("오류 발생: " + e.getMessage());
+        }
     }
 }
---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class StringCalculator {
+    StringCalculator() {
+    }
+
+    public int add(String text) {
+        // 1. 핵심 요구사항 구현: null 또는 빈 문자열 체크.
+        if (text == null || text.isEmpty()) return 0; // 0을 반환한다.
+    }
+
+    // TODO: 향후 여기에 파싱, 예외처리, 덧셈 로직이 추가됩니다.
+
+    //현재 단계에서는 임의로 0 반환되도록 처리한다.
+    retrun 0;
+}
