cf1b1ed5c85f85eb1a5296811e220bd43580aceb
이경준 <kyoungjunlee@igyeongjun-ui-MacBookPro.local>
2025-10-18 14:46:10 +0900

    핵심기능 3번 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String text = Console.readLine();
 
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,25 +1,27 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     StringCalculator() {
     }
 
-    // TODO: 향후 여기에 파싱, 예외처리, 덧셈 로직이 추가됩니다.
     public int add(String text) {
         int sum = 0;
 
-        // text가 null이거나 비워있으면 0을 반환한다.
-        if (text == null || text.isEmpty()){ return 0; }
+        // null 또는 빈 문자열일 경우 0을 반환한다
+        if (text == null || text.isEmpty()){
+            return 0;
+        }
 
-        // 기본 구분자 ,와 : 관리
-        String[] numbers = text.split("[,:]");
+        String[] numbers = splitByCustomDelimiter(text);
 
         for (String number : numbers) {
             try {
-                //여기서 예외 발생 시 catch로 이동한다.
+                //예외 발생 시 catch로 이동한다
                 int numberValue = Integer.parseInt(number);
 
-                //음수체크 로직.
+                //음수체크 로직
                 if (numberValue < 0) {
                     throw new IllegalArgumentException("음수(" + numberValue + ")는 사용할 수 없습니다.");
                 }
@@ -31,5 +33,26 @@ public class StringCalculator {
             }
         }
         return sum;
+    }
+    //커스텀 구분자를 처리하고 숫자 문자열 배열을 반환하는 역할
+    private String[] splitByCustomDelimiter(String text){
+    // 커스텀 구분자가 없을 경우 기본 구분자
+    String delimiter ="[,:]";
+    String numberString = text;
+    // 커스텀 구분자 패턴 확인
+    if (text.startsWith("//") && text.contains("\n")){
+        int firstNewLineIndex = text.indexOf("\n");
+
+        // '//' 다음(인덱스 2)부터 '\n' 전까지의 문자를 커스텀 구분자로 추출
+        delimiter = text.substring(2, firstNewLineIndex);
+
+        // '\n' 다음 위치부터 실제 계산할 숫자 문자열로 지정
+        numberString = text.substring(firstNewLineIndex+1);
+
+        //Pattern.quote문을 사용하여 커스텀 구분자를 안전하게 인용한다(정규표현식은 오류를 발생시킬 수 있기 때문.)
+        delimiter = Pattern.quote(delimiter) + "|[,:]";
+    }
+
+    return numberString.split(delimiter);
     }
 }
